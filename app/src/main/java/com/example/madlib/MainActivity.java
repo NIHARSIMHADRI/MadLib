35ccf897b2dc287c5afeeb985d4e5eff86b4304f
simhadri4025 <simhadri4025@students.d211.org>
2019-09-03 13:58:32 -0500

    color change and transitions

--- app/src/main/java/com/example/madlib/MainActivity.java
+++ app/src/main/java/com/example/madlib/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,29 +17,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendInfo(View v) {
+        Intent intent = null;
+        try {
 // getting a reference to my edit text fields
-        EditText myNoun = (EditText) findViewById(R.id.myNoun);
-        EditText myAdjective = (EditText) findViewById(R.id.myAdjective);
-        EditText myVerb = (EditText) findViewById(R.id.myVerb);
-        EditText myAnimal = (EditText) findViewById(R.id.myAnimal);
-        EditText myNumber = (EditText) findViewById(R.id.myNumber);
+            EditText myNoun = (EditText) findViewById(R.id.myNoun);
+            EditText myAdjective = (EditText) findViewById(R.id.myAdjective);
+            EditText myVerb = (EditText) findViewById(R.id.myVerb);
+            EditText myAnimal = (EditText) findViewById(R.id.myAnimal);
+            EditText myNumber = (EditText) findViewById(R.id.myNumber);
 // extracting the text from those edit text fields
-        String myNounStr = myNoun.getText().toString();
-        String myAdjectivestr = myAdjective.getText().toString();
-        String myVerbstr = myVerb.getText().toString();
-        String myAnimalstr = myAnimal.getText().toString();
-        int myNumberstr = (int) Integer.parseInt(String.valueOf(myNumber));
+            String myNounStr = myNoun.getText().toString();
+            String myAdjectivestr = myAdjective.getText().toString();
+            String myVerbstr = myVerb.getText().toString();
+            String myAnimalstr = myAnimal.getText().toString();
+            String myNumberstr = myNumber.getText().toString();
 // Creating the intent object so I can send data
-        Intent intent = new Intent(this, Story.class);
+            intent = new Intent(this, Story.class);
 // putting data from edit text fields into intent to send to other activity
 // MY_Name and MY_AGE are public constants in InfoActivity class
-        intent.putExtra(Story.MY_NOUN, myNounStr);
-        intent.putExtra(Story.MY_ADJECTIVE, myAdjectivestr);
-        intent.putExtra(Story.MY_VERB, myVerbstr);
-        intent.putExtra(Story.MY_ANIMAL, myAnimalstr);
-        intent.putExtra(Story.MY_NUMBER, myNumberstr);
-
+            intent.putExtra(Story.MY_NOUN, myNounStr);
+            intent.putExtra(Story.MY_ADJECTIVE, myAdjectivestr);
+            intent.putExtra(Story.MY_VERB, myVerbstr);
+            intent.putExtra(Story.MY_ANIMAL, myAnimalstr);
+            intent.putExtra(Story.MY_NUMBER, myNumberstr);
 // loads the next activity
-        startActivity(intent);
+            startActivity(intent);
+        } catch (Exception e) {
+            intent.putExtra(Story.MY_NOUN, "error");
+            intent.putExtra(Story.MY_ADJECTIVE, "error");
+            intent.putExtra(Story.MY_VERB, "error");
+            intent.putExtra(Story.MY_ANIMAL, "error");
+            intent.putExtra(Story.MY_NUMBER, "error");
+        }
     }
 }
--- app/src/main/java/com/example/madlib/Story.java
+++ app/src/main/java/com/example/madlib/Story.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class Story extends AppCompatActivity {
@@ -16,18 +17,19 @@ public class Story extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_story);
-        Intent intent = getIntent();
-        String myNoun = intent.getStringExtra(MY_NOUN);
-        String myAdjective = intent.getStringExtra(MY_ADJECTIVE);
-        String myVerb = intent.getStringExtra(MY_VERB);
-        String myAnimal = intent.getStringExtra(MY_ANIMAL);
-        String myNumber = intent.getStringExtra(MY_NUMBER);
-        String strToDisplay = "In a jungle there was a huge " + myNoun + ". The " + myNoun + " liked to "
-                + myVerb + " every day and was hated by everyone else. One day he found a " +
-                myAdjective + " apple and decided to share it with a " + myAnimal + ". Due to his generosity he was rewarded with " + myNumber + " dollars.";
-        TextView str = (TextView) findViewById(R.id.story);
-        str.setText(strToDisplay);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_story);
+            Intent intent = getIntent();
+            String myNoun = intent.getStringExtra(MY_NOUN);
+            String myAdjective = intent.getStringExtra(MY_ADJECTIVE);
+            String myVerb = intent.getStringExtra(MY_VERB);
+            String myAnimal = intent.getStringExtra(MY_ANIMAL);
+            String myNumber = intent.getStringExtra(MY_NUMBER);
+            String strToDisplay = "In a jungle there was a huge " + myNoun + ". The " + myNoun + " liked to "
+                    + myVerb + " every day and was hated by everyone else. One day he found a " +
+                    myAdjective + " apple and decided to share it with a " + myAnimal + ". Due to his generosity he was rewarded with " + myNumber + " dollars.";
+
+           TextView str = (TextView) findViewById(R.id.story);
+            str.setText(strToDisplay);
     }
 }
